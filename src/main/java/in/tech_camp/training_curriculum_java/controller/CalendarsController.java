c0c7ab4b18c5764640e6cdf55bfcf3c3d5bcdd12
shots0824 <shota.shimizu@smart-group.co.jp>
2026-06-01 10:36:51 +0900

    Issue5~6

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -66,9 +66,15 @@ public class CalendarsController {
           }
       }
 
+
+      day_map.put("month",currentDate.getMonthValue());
+      int wdayNum = (todaysDate.getDayOfWeek().getValue() + x) % 7;
+      
+
       day_map.put("month", currentDate.getMonthValue());
       day_map.put("date", currentDate.getDayOfMonth());
       day_map.put("plans", todayPlans);
+      day_map.put("wday", wdays[wdayNum]);
 
       weekDays.add(day_map);
     }
